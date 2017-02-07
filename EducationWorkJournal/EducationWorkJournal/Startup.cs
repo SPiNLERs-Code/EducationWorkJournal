a7beeb0616ee61bd9300c6c28af9c6ccad9aa088
bryan albrecht <bryan.albrecht@noser.com>
2017-02-07 11:43:01 +0100

    Createt project

---
+++ EducationWorkJournal/EducationWorkJournal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EducationWorkJournal.Startup))]
+namespace EducationWorkJournal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
